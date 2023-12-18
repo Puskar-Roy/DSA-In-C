592c158d65f131131f0a05501270df0916314fb6
Puskar-Roy <puskarroy600@gmail.com>
2023-12-19 01:15:43 +0530

    feat : stack linked list

--- stackLinkList.c
+++ stackLinkList.c
@@ -2,18 +2,68 @@
 #include <stdlib.h>
 #define size 10
 
-
-typedef struct node{
+typedef struct node
+{
     int data;
     struct node *next;
-}node;
+} node;
 
-typedef struct stack{
+typedef struct stack
+{
     struct node *top;
-}stack;
+} stack;
 
-
-int push(stack *s,int n){
+int push(stack *s, int n)
+{
     node *x;
-    x = ()
+    x = (node *)malloc(sizeof(node));
+    x->data = n;
+    x->next = s->top;
+    s->top = x;
+    return 1;
+}
+
+int pop(stack *s, int a)
+{
+    node *t;
+    if (s->top == NULL)
+    {
+        return 0;
+    }
+    *a = s->top->data;
+    t = s->top;
+    s->top = s->top->next;
+    free(t);
+    return 1;
+}
+
+void display(stack *s)
+{
+    if (s->top == NULL)
+    {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    node *current = s->top;
+    printf("Stack elements: ");
+    while (current != NULL)
+    {
+        printf("%d", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    stack s;
+    s.top = NULL;
+
+    push(&s, 10);
+    push(&s, 20);
+    push(&s, 30);
+
+
+    return 0;
 }
